667732e9061a840bfc6c8e323a017d66e75b1312
Michael <mobrien@colgate.edu>
2020-05-26 17:15:42 -0400

    changed website title to my name, set up basic structure ofmy about me page

--- src/about-me.js
+++ src/about-me.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import ProfilePic from "./IMG_0808.png";
+import "./App.css";
 
 const AboutPage = () => {
   let history = useHistory();
@@ -11,6 +13,32 @@ const AboutPage = () => {
     <div>
       About Page
       <div onClick={returnToHome}>Return to Home</div>
+      <div className="about-body">
+        <div className="bio">
+          <div className="profile-pic">
+            <img id="prof-pic" src={ProfilePic} alt="" />
+          </div>
+          <div id="personal-bio">
+            <h4>Blurb</h4>
+            <p>things about me</p>
+          </div>
+          <div id="skills"></div>
+          <div id="links">
+            <a href="https://www.github.com/mobrien829" className="github">
+              Github
+            </a>
+            <a href="" className="email">
+              Email me!
+            </a>
+            <a
+              href="https://www.linkedin.com/in/mobrien829"
+              className="linkedin"
+            >
+              LinkedIn
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

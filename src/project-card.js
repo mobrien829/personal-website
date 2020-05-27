39dcdae437654c82c1511990c3cd3ee27a1dc802
Michael <mobrien@colgate.edu>
2020-05-27 17:51:40 -0400

    basic image and text working for cards

--- src/project-card.js
+++ src/project-card.js
@@ -8,9 +8,23 @@ import {
   CardActionArea,
   Button,
 } from "@material-ui/core";
+import ProfilePic from "./IMG_0808.png";
+
+const useStyles = makeStyles({
+  card: { maxWidth: 345 },
+  media: { height: 140 },
+});
 
 const ProjectCard = () => {
-  return null;
+  const classes = useStyles();
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia className={classes.media} image={ProfilePic} />
+        <CardContent></CardContent>
+      </CardActionArea>
+    </Card>
+  );
 };
 
 export default ProjectCard;
--- src/projects-grid.js
+++ src/projects-grid.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
+import ProjectCard from "./project-card";
 
 const ProjectGrid = () => {
   return (
     <Grid container spacing={4} style={{ padding: "24px" }}>
       <Grid key={1} item xs={12} sm={6} md={4} lg={4} xl={3}>
-        <div>hello</div>
+        <ProjectCard />
       </Grid>
       <Grid key={2} item xs={12} sm={6} md={4} lg={4} xl={3}>
         <div>hello</div>

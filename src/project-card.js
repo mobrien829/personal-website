5c6b0b1c692a71d5904f1f323c7eb1904d224813
Michael <mobrien@colgate.edu>
2020-05-27 17:39:44 -0400

    config for individual project cards set up

---
+++ src/project-card.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Card,
+  CardMedia,
+  CardActions,
+  CardContent,
+  CardActionArea,
+  Button,
+} from "@material-ui/core";
+
+const ProjectCard = () => {
+  return null;
+};
+
+export default ProjectCard;

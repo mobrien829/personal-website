7ddeceaf42b0de2c8656fd76949d299ff1869557
Michael <mobrien@colgate.edu>
2020-06-01 16:26:33 -0400

    added weathering hooks to portfolio

--- src/projects-grid.js
+++ src/projects-grid.js
@@ -3,12 +3,22 @@ import { Grid } from "@material-ui/core";
 import ProjectCard from "./project-card";
 import ProfilePic from "./IMG_0808.png";
 import WaterPic from "./hydration-image.png";
+import WeatherPic from "./weathering-hooks.png";
+import "./App.css";
 
 const ProjectGrid = () => {
   return (
     <Grid container spacing={4} style={{ padding: "24px" }}>
       <Grid key={1} item xs={12} sm={6} md={4} lg={4} xl={3}>
-        <ProjectCard thumbnail={ProfilePic} />
+        <ProjectCard
+          thumbnail={WeatherPic}
+          projectName={"Weathering Hooks"}
+          liveDemo={"https://hooks-n-weather.herokuapp.com/"}
+          githubUrl={"https://github.com/mobrien829/react-weather-hooks"}
+          description={
+            "React hook based weather app using an Express server to route the weather requests. Built using React and Express with the Mapbox and WeatherStack APIs."
+          }
+        />
       </Grid>
       <Grid key={2} item xs={12} sm={6} md={4} lg={4} xl={3}>
         <ProjectCard
@@ -16,10 +26,13 @@ const ProjectGrid = () => {
           projectName={"Hydration Helper"}
           liveDemo={"https://hydration-helper.herokuapp.com/"}
           githubUrl={"https://github.com/mobrien829/hydration-extension"}
+          description={
+            "React based webapp for tracking when and how many cups of water you've consumed in a day. Currently awaiting approval as a Chrome extension."
+          }
         />
       </Grid>
       <Grid key={3} item xs={12} sm={6} md={4} lg={4} xl={3}>
-        <div>hello</div>
+        <ProjectCard />
       </Grid>
     </Grid>
   );

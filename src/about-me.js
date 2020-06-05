7d4a941cea7a0a855c42be58829a664f848943a4
Michael <mobrien@colgate.edu>
2020-06-05 17:45:21 -0400

    attempted to style about page, added boxes for use in flexbox

--- src/about-me.js
+++ src/about-me.js
@@ -2,43 +2,48 @@ import React from "react";
 import ProfilePic from "./IMG_0808.png";
 import "./App.css";
 import NavBar from "./navbar";
-import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography, Avatar, Box } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  profPic: {
+    width: theme.spacing(16),
+    height: theme.spacing(16),
+  },
+}));
 
 const AboutPage = () => {
+  const classes = useStyles();
+
   return (
-    <div>
+    <>
       <NavBar />
-      <div className="about-body">
-        <div className="bio">
-          <div className="profile-pic">
-            <img id="prof-pic" src={ProfilePic} alt="" />
-          </div>
-          <div id="personal-bio">
-            <Typography variant="h5">About Me</Typography>
-            <Typography variant="p">
-              I'm an aspiring full-stack web developer currently searching for a
-              role that to launch my career. When I'm not improving my skills, I
-              spend my time gaming and playing Ultimate Frisbee.
-            </Typography>
-          </div>
-          <div id="skills"></div>
-          <div id="links">
-            <a href="https://www.github.com/mobrien829" className="github">
-              Github
-            </a>
-            <a href="mailto:mobrien0829@gmail.com" className="email">
-              Email me!
-            </a>
-            <a
-              href="https://www.linkedin.com/in/mobrien829"
-              className="linkedin"
-            >
-              LinkedIn
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+      <Box display="flex" flexDirection="column">
+        <Box>
+          <Typography variant="h5">About</Typography>
+          <Avatar alt="img" src={ProfilePic} className={classes.profPic} />
+          <Typography variant="p">
+            I'm an aspiring full-stack web developer currently searching for a
+            role that to launch my career. When I'm not improving my skills, I
+            spend my time gaming and playing Ultimate Frisbee.
+          </Typography>
+        </Box>
+        <Box justifyContent="center">
+          <a href="https://www.github.com/mobrien829" className="github">
+            Github
+          </a>
+          <a href="mailto:mobrien0829@gmail.com" className="email">
+            Email me!
+          </a>
+          <a href="https://www.linkedin.com/in/mobrien829" className="linkedin">
+            LinkedIn
+          </a>
+        </Box>
+      </Box>
+    </>
   );
 };
 

c823b9e9a870b7d42981d18b4a6dfd4dcda0aed6
Michael <mobrien@colgate.edu>
2020-06-16 23:23:47 -0400

    added icons for socials/github, styled as well

--- src/about-me.js
+++ src/about-me.js
@@ -4,6 +4,8 @@ import "./App.css";
 import NavBar from "./navbar";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography, Avatar, Box } from "@material-ui/core";
+// import GitHubIcon from "@material-ui/icons/GitHub";
+import { GitHub, Twitter, LinkedIn, MailOutline } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,6 +15,18 @@ const useStyles = makeStyles((theme) => ({
     width: theme.spacing(16),
     height: theme.spacing(16),
   },
+  bio: {
+    textAlign: "center",
+  },
+  github: {
+    color: "black",
+  },
+  twitter: {
+    color: "#00ACEE",
+  },
+  linkedIn: {
+    color: "#0E76A8",
+  },
 }));
 
 const AboutPage = () => {
@@ -21,25 +35,28 @@ const AboutPage = () => {
   return (
     <>
       <NavBar />
-      <Box display="flex" flexDirection="column">
+      <Box display="flex" justifyContent="center" minHeight="100vh">
         <Box>
           <Typography variant="h5">About</Typography>
           <Avatar alt="img" src={ProfilePic} className={classes.profPic} />
-          <Typography variant="p">
+          <Typography variant="p" className={classes.bio}>
             I'm an aspiring full-stack web developer currently searching for a
             role that to launch my career. When I'm not improving my skills, I
             spend my time gaming and playing Ultimate Frisbee.
           </Typography>
         </Box>
-        <Box justifyContent="center">
+        <Box alignSelf="center" justifyContent="center">
           <a href="https://www.github.com/mobrien829" className="github">
-            Github
+            <GitHub className={classes.github} />
           </a>
           <a href="mailto:mobrien0829@gmail.com" className="email">
-            Email me!
+            <MailOutline />
           </a>
           <a href="https://www.linkedin.com/in/mobrien829" className="linkedin">
-            LinkedIn
+            <LinkedIn className={classes.linkedIn} />
+          </a>
+          <a href="https://www.twitter.com/mobrien829" className="twitter">
+            <Twitter className={classes.twitter} />
           </a>
         </Box>
       </Box>
